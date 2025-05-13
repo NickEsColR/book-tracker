84b2fcce65baec4df8fb72d419cd168fdb219fd7
NickEsColR <nicoesol0818@gmail.com>
2025-05-13 23:38:34 +0000

    refactor: enhance book retrieval and pagination functionality; update getBooks API to support offset and limit parameters

--- src/utils/booksApi.ts
+++ src/utils/booksApi.ts
@@ -2,11 +2,11 @@ import type { OpenLibraryResponse } from "@/types/OpenLibraryTypes";
 
 const BASE_URL = "http://openlibrary.org/search.json?fields=key,title,cover_i,author_name";
 
-export const getBooks = async (title?: string | null, authors?: string | null, genres?: string | null): Promise<OpenLibraryResponse> => {
+export const getBooks = async (title?: string | null, authors?: string | null, genres?: string | null, offset?: number, limit?: number): Promise<OpenLibraryResponse> => {
   if (!title && !authors && !genres) {
     title = "all";
   }
-  const url = `${BASE_URL}${title ? `&title=${title}` : ''}${authors ? `&author=${authors}` : ''}${genres ? `&genre=${genres}` : ''}&sort=rating`;
+  const url = `${BASE_URL}${title ? `&title=${title}` : ''}${authors ? `&author=${authors}` : ''}${genres ? `&genre=${genres}` : ''}${offset !== undefined ? `&offset=${offset}` : ''}${limit !== undefined ? `&limit=${limit}` : ''}&sort=rating`;
   const response = await fetch(url);
   if (!response.ok) {
     throw new Error('Failed to fetch books');
