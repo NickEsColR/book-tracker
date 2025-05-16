fcaf318550ac23d054f4c54776c2cb0f63aa5b33
NickEsColR <nicoesol0818@gmail.com>
2025-05-16 20:37:50 +0000

    feat: add Clerk authentication middleware and update Header component for user sign-in functionality

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,13 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 import netlify from '@astrojs/netlify';
+import clerk from "@clerk/astro";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
-
+  integrations: [clerk()],
   adapter: netlify()
 });
