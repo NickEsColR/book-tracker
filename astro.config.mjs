afd64cf7e856a71f31c58dd7df755dfc8b55f647
NickEsColR <nicoesol0818@gmail.com>
2025-05-18 01:30:12 +0000

    feat: integrate @astrojs/db and create initial database configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,18 +8,18 @@ import clerk from "@clerk/astro";
 import { dark } from "@clerk/themes";
 import {esMX} from "@clerk/localizations";
 
+import db from "@astrojs/db";
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  integrations: [
-    clerk({
-      appearance: {
-        baseTheme: dark,
-      },
-      localization: esMX,
-    }),
-  ],
+  integrations: [clerk({
+    appearance: {
+      baseTheme: dark,
+    },
+    localization: esMX,
+  }), db()],
   adapter: netlify(),
 });
